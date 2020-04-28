b3f8f34b74731c67ad2e36a1e2a3677c275060e1
Bruce Durling <bruce@mastodonc.com>
2020-04-28 14:58:53 +0100

    Get rid of tick doubling

--- src/witan/send/chart.clj
+++ src/witan/send/chart.clj
@@ -59,10 +59,30 @@
 
 
 (defn zero-index-numerical-y-axes [prepped-data]
-  (let [[t v] (get-in prepped-data [:extents :y 0])]
+  (let [[t [_bottom top]] (get-in prepped-data [:extents :y 0])]
     (if (= :numerical t)
-      (plotb/update-scale prepped-data :y :domain [0 (second v)])
+      (plotb/update-scale prepped-data :y :domain [0 (+ (* 0.025 top) top)])
       prepped-data)))
+
+(defn update-chart-y-axis-ticks [chart-spec series]
+  (let [y-max (->> series
+                   (mapcat (fn [x] (second x)))
+                   (map (fn [y] (second y)))
+                   (reduce max))
+        [t [_bottom _top]] (get-in chart-spec [:extents :y 0])]
+    (if (and (= :numerical t) (< y-max 10))
+      (plotb/update-scale chart-spec :y :domain [0 10])
+      chart-spec)))
+
+(defn update-chart-x-axis-ticks [chart-spec series]
+  (let [x-count (->> series
+                     (mapcat (fn [x] (second x)))
+                     (map (fn [y] (first y)))
+                     (into #{})
+                     count)]
+    (if (< x-count 10)
+      (plotb/update-scale chart-spec :x :ticks x-count)
+      chart-spec)))
 
 (defn zero-y-index [{:keys [x-axis y-axis legend title series size]}]
   (let [size (or size {:width 1539 :height 1037 :background (color/color :white)}) ;; 1539x1037 is almost exactly the right size to go into the slide
@@ -70,9 +90,10 @@
     (-> (apply plotb/series (into [[:grid]] series))
         (plotb/preprocess-series)
         (plotb/update-scale :x :fmt (:tick-formatter x-axis))
-        ;;(plotb/update-scale :x :ticks 10)
+        (update-chart-x-axis-ticks series)
         (plotb/update-scale :y :fmt (:tick-formatter y-axis))
         (zero-index-numerical-y-axes)
+        (update-chart-y-axis-ticks series)
         (plotb/add-axes :bottom {:ticks {:font-size 24 :font-style nil}})
         (plotb/add-axes :left {:ticks {:font-size 24 :font-style nil}})
         (plotb/add-label :bottom (:label x-axis) {:font-size 36 :font "Open Sans" :font-style nil})
