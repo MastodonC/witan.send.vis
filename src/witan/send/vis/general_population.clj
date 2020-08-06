c3a3ac63ad64fdf3b22ef8b92538a798ae6b6905
Seb Bailey <sebastian@mastodonc.com>
2020-08-06 14:39:34 +0100

    Remove old chart code

--- src/witan/send/vis/general_population.clj
+++ src/witan/send/vis/general_population.clj
@@ -51,18 +51,3 @@
    (charts ay/ay-lookup
            historical-data
            gen-pop-titles-and-sets)))
-
-;; TODO? Still needed?
-(defn total-population [total-population]
-  {:x-axis {:tick-formatter int :label "Calendar Year" :format {:font-size 24 :font "Open Sans"}}
-   :y-axis {:tick-formatter int :label "Population" :format {:font-size 24 :font "Open Sans"}}
-   :legend {:label "Population"
-            :legend-spec [[:line "Total"
-                           {:color :blue :stroke {:size 4} :shape \^ :font "Open Sans" :font-size 36}]]} ;; flip shape in legend
-   :title  {:label "Total General Population"
-            :format {:font-size 24 :font "Open Sans" :margin 36 :font-style nil}}
-   :series [(wss/maps->line {:x-key :calendar-year
-                             :y-key :population
-                             :color wsc/blue
-                             :point \V}
-                            total-population)]})
