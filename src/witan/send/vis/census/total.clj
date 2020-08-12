835b5bd58129acbbf7d6ec55a133a270e8148a09
Seb Bailey <sebastian@mastodonc.com>
2020-08-12 12:42:21 +0100

    Add SEN2 population to chart

--- src/witan/send/vis/census/total.clj
+++ src/witan/send/vis/census/total.clj
@@ -3,23 +3,6 @@
             [witan.send.series :as wss]
             [witan.phyrexian.sen2-processing-tool :as sen]))
 
-(defn total-send-population [total-population]
-  {:x-axis {:tick-formatter int :label "Calendar Year" :format {:font-size 24 :font "Open Sans"}}
-   :y-axis {:tick-formatter int :label "Population" :format {:font-size 24 :font "Open Sans"}}
-   :legend {:label "Population"
-            :legend-spec [[:line "Total"
-                           {:color wsc/blue :stroke {:size 4} :shape \^ :font "Open Sans" :font-size 36}]]} ;; flip shape in legend
-   :title  {:label "Total SEND Population"
-            :format {:font-size 24 :font "Open Sans" :margin 36 :font-style nil}}
-   :series [(wss/maps->line {:x-key :calendar-year
-                             :y-key :population
-                             :color wsc/blue
-                             :point \V}
-                            total-population)]})
-
-(defn chart [title total-send]
-  [(wsc/comparison-chart-and-table (assoc total-send :title title))])
-
 (defn key-to-year [k]
   (read-string (str "20" (-> k key name (clojure.string/split #"-") second))))
 
@@ -29,3 +12,27 @@
     (map #(assoc {}
                  :calendar-year (key-to-year %)
                  :population (val %)) pop)))
+
+(defn total-send-population [total-population sen2]
+  {:x-axis {:tick-formatter int :label "Calendar Year" :format {:font-size 24 :font "Open Sans"}}
+   :y-axis {:tick-formatter int :label "Population" :format {:font-size 24 :font "Open Sans"}}
+   :legend {:label "Population"
+            :legend-spec [[:line "SEN2"
+                           {:color wsc/orange :stroke {:size 4} :shape \^ :font "Open Sans" :font-size 36}]
+                          [:line "Total"
+                           {:color wsc/blue :stroke {:size 4} :shape \^ :font "Open Sans" :font-size 36}]]} ;; flip shape in legend
+   :title  {:label "Total SEND Population"
+            :format {:font-size 24 :font "Open Sans" :margin 36 :font-style nil}}
+   :series [(wss/maps->line {:x-key :calendar-year
+                             :y-key :population
+                             :color wsc/orange
+                             :point \V}
+                            sen2)
+            (wss/maps->line {:x-key :calendar-year
+                             :y-key :population
+                             :color wsc/blue
+                             :point \V}
+                            total-population)]})
+
+(defn chart [title total-send]
+  [(wsc/comparison-chart-and-table (assoc total-send :title title))])
