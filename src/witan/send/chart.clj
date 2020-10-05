7f5791ba153670887850fb2dfed9e3984a9efc44
Seb Bailey <sebastian@mastodonc.com>
2020-10-05 15:12:39 +0100

    refactor insert-zero-counts

--- src/witan/send/chart.clj
+++ src/witan/send/chart.clj
@@ -388,8 +388,8 @@
   (xl/save-workbook! file-name wb))
 
 (defn insert-zero-counts [census-data counts]
-  (mapcat (fn [cy] (map #(if (nil? (some #{%} (map :academic-year (val cy))))
-                           {:calendar-year (:calendar-year (-> cy val first)), :academic-year %, :population 0}
-                           (first (filter (fn [ay] (= % (:academic-year ay))) (val cy))))
+  (mapcat (fn [cy] (map (fn [ay] (if-let [ay-map (some (fn [m] (when (#{ay} (:academic-year m)) m)) (val cy))]
+                                   ay-map
+                                   {:calendar-year (:calendar-year (-> cy val first)), :academic-year ay, :population 0}))
                         (into (sorted-set) (map :academic-year census-data))))
           (group-by :calendar-year counts)))
