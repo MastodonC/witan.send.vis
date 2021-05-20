819779ea5d6e7dd7fc74d33e14b4fc4fd8858fcc
Seb Bailey <sebastian@mastodonc.com>
2021-05-20 16:24:27 +0100

    Remove unused key

--- src/witan/send/vis/output_setting_cost.clj
+++ src/witan/send/vis/output_setting_cost.clj
@@ -25,7 +25,6 @@
   (let [domain-key :setting]
     (wsc/domain-charts {:domain-key domain-key
                         :chart-base-def (base-setting-chart-def {})
-                        :serie-base-def base-setting-serie-def
                         :colors-and-points (wsc/domain-colors-and-points domain-key projection-data)
                         :projection-data projection-data}
                        titles-and-sets)))
